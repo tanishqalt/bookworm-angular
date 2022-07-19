567b6761b7dad5b03cf64fc4a2474558f456d453
Tanishq Sharma <tanishqsh@gmail.com>
2022-07-18 22:46:42 -0400

    Library components

--- src/app/components/library/list-of-users/list-of-users.component.ts
+++ src/app/components/library/list-of-users/list-of-users.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list-of-users',
   templateUrl: './list-of-users.component.html',
-  styleUrls: ['./list-of-users.component.css']
+  styleUrls: ['./list-of-users.component.css'],
 })
 export class ListOfUsersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
